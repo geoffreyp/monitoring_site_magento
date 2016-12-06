f2365d8c3f0db393692d403e6ba1e7cfdf61adff
Geoffrey <geoffrey.pruvost@hotmail.com>
2016-12-06 13:26:58 +0100

    Add guzzle & print opened ticket on dashbord

---
+++ controller/redmine.php
@@ -0,0 +1,19 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+
+function getNbTicket($apikey){
+
+    $client = new GuzzleHttp\Client(['base_uri' => 'http://www.hostedredmine.com/']);
+    $response = $client->request('GET', 'http://www.hostedredmine.com/issues.json?project_id=43188&status_id=opened',
+        ['headers' => ['X-Redmine-API-Key' => $apikey]]);
+
+    $json = json_decode($response->getBody());
+
+    //echo ($json->{'total_count'});
+    return $json->{'total_count'};
+}
+
+getNbTicket("c232cdf169899c7c6074eecf42f7827ae37be34e");
