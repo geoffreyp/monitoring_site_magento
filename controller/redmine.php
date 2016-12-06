d474936c1ad6020486cc27034d375e1be2c70171
Florian Vansteene <florian.vansteene@gmail.com>
2016-12-06 18:20:45 +0100

    Add number of tickets for each groups

--- controller/redmine.php
+++ controller/redmine.php
@@ -15,3 +15,41 @@ function getNbTicket($apikey){
     //echo ($json->{'total_count'});
     return $json->{'total_count'};
 }
+
+function getNbTicketByGroup($apikey, $group){
+
+    $nb = 0;
+
+    $client = new GuzzleHttp\Client(['base_uri' => 'http://www.hostedredmine.com/']);
+
+    $response1 = $client->request('GET', 'http://www.hostedredmine.com/projects/ulco2016project1/memberships.json',
+        ['headers' => ['X-Redmine-API-Key' => $apikey]]);
+
+    $json1 = json_decode($response1->getBody());
+
+    $nbElements  = $json1->{'total_count'};;
+
+
+
+    for ($i=0; $i<$nbElements; $i++) {
+
+        $currentGroup = $json1->memberships[$i]->roles[0]->id;
+
+        if ($currentGroup == $group){
+
+            $currentUser = $json1->memberships[$i]->user->id;
+            $response2 = $client->request('GET', 'http://www.hostedredmine.com/issues.json?project_id=43188&assigned_to_id='.$currentUser.'&status_id=opened',
+                ['headers' => ['X-Redmine-API-Key' => $apikey]]);
+
+            $json2 = json_decode($response2->getBody());
+
+            $nb = $nb + intval($json2->{'total_count'});
+
+
+        }
+    }
+    return $nb;
+}
+
+getNbTicketByGroup("c232cdf169899c7c6074eecf42f7827ae37be34e",4);
+
--- index.php
+++ index.php
@@ -886,49 +886,19 @@ require('controller/redmine.php');
           </div>
           <!-- /.box -->
 
-          <!-- solid sales graph -->
-          <div class="box box-solid bg-teal-gradient">
-            <div class="box-header">
-              <i class="fa fa-th"></i>
-
-              <h3 class="box-title">Sales Graph</h3>
-
-              <div class="box-tools pull-right">
-                <button type="button" class="btn bg-teal btn-sm" data-widget="collapse"><i class="fa fa-minus"></i>
-                </button>
-                <button type="button" class="btn bg-teal btn-sm" data-widget="remove"><i class="fa fa-times"></i>
-                </button>
-              </div>
-            </div>
-            <div class="box-body border-radius-none">
-              <div class="chart" id="line-chart" style="height: 250px;"></div>
-            </div>
-            <!-- /.box-body -->
-            <div class="box-footer no-border">
-              <div class="row">
-                <div class="col-xs-4 text-center" style="border-right: 1px solid #f4f4f4">
-                  <input type="text" class="knob" data-readonly="true" value="20" data-width="60" data-height="60" data-fgColor="#39CCCC">
-
-                  <div class="knob-label">Mail-Orders</div>
+            <div class="box box-widget widget-user-2">
+                <!-- Add the bg color to the header using any of the bg-* classes -->
+                <div class="widget-user-header bg-yellow">
+                    <!-- /.widget-user-image -->
+                    <h3 class="widget-user-username">Ticket Opened By Group</h3>
                 </div>
-                <!-- ./col -->
-                <div class="col-xs-4 text-center" style="border-right: 1px solid #f4f4f4">
-                  <input type="text" class="knob" data-readonly="true" value="50" data-width="60" data-height="60" data-fgColor="#39CCCC">
-
-                  <div class="knob-label">Online</div>
+                <div class="box-footer no-padding">
+                    <ul class="nav nav-stacked">
+                        <li><a href="#">Manager <span class="pull-right badge bg-blue"><?php echo getNbTicketByGroup("c232cdf169899c7c6074eecf42f7827ae37be34e",3); ?></span></a></li>
+                        <li><a href="#">Developer <span class="pull-right badge bg-red"><?php echo getNbTicketByGroup("c232cdf169899c7c6074eecf42f7827ae37be34e",4); ?></span></a></li>
+                    </ul>
                 </div>
-                <!-- ./col -->
-                <div class="col-xs-4 text-center">
-                  <input type="text" class="knob" data-readonly="true" value="30" data-width="60" data-height="60" data-fgColor="#39CCCC">
-
-                  <div class="knob-label">In-Store</div>
-                </div>
-                <!-- ./col -->
-              </div>
-              <!-- /.row -->
             </div>
-            <!-- /.box-footer -->
-          </div>
           <!-- /.box -->
 
           <!-- Calendar -->
