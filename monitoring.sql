ddbd6578e8dacaf8f8652689af7f189ef446c127
Florian Vansteene <florian.vansteene@gmail.com>
2016-12-07 14:55:16 +0100

    Add sql file

---
+++ monitoring.sql
@@ -0,0 +1,242 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  localhost
+-- Généré le :  Mer 07 Décembre 2016 à 14:37
+-- Version du serveur :  5.6.33
+-- Version de PHP :  7.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Base de données :  `monitoring`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commit`
+--
+
+CREATE TABLE `commit` (
+  `id` varchar(100) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `titre` varchar(100) NOT NULL,
+  `commentaire` varchar(250) DEFAULT NULL,
+  `date_cree` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `notification`
+--
+
+CREATE TABLE `notification` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(250) DEFAULT NULL,
+  `importance` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `priorite`
+--
+
+CREATE TABLE `priorite` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `priorite`
+--
+
+INSERT INTO `priorite` (`id`, `name`) VALUES
+(3, 'Low'),
+(4, 'Normal'),
+(5, 'High'),
+(6, 'Urgent'),
+(7, 'Immediate');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `projet`
+--
+
+CREATE TABLE `projet` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(250) DEFAULT NULL,
+  `ssh_url` varchar(250) DEFAULT NULL,
+  `http_url` varchar(250) DEFAULT NULL,
+  `web_url` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `statut`
+--
+
+CREATE TABLE `statut` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `statut`
+--
+
+INSERT INTO `statut` (`id`, `name`) VALUES
+(1, 'New'),
+(2, 'Assigned'),
+(3, 'Resolved'),
+(4, 'Approved'),
+(5, 'Closed'),
+(6, 'Rejected'),
+(7, 'Blocked'),
+(8, 'In Progress');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `statut_id` int(11) NOT NULL,
+  `priorite_id` int(11) NOT NULL,
+  `tracker_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `date_cree` varchar(50) NOT NULL,
+  `date_fin` varchar(50) DEFAULT NULL,
+  `date_modif` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tracker`
+--
+
+CREATE TABLE `tracker` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `tracker`
+--
+
+INSERT INTO `tracker` (`id`, `name`) VALUES
+(1, 'Bug'),
+(2, 'Feature'),
+(3, 'Support'),
+(4, 'Task');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
+  `id` int(11) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `mail` varchar(200) NOT NULL,
+  `last_login_on` date DEFAULT NULL,
+  `date_cree` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id`, `login`, `password`, `firstname`, `lastname`, `mail`, `last_login_on`, `date_cree`) VALUES
+(1, 'root', 'toto', 'root', 'toto', 'toto@root.com', NULL, '2016-12-06');
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `commit`
+--
+ALTER TABLE `commit`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `notification`
+--
+ALTER TABLE `notification`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `priorite`
+--
+ALTER TABLE `priorite`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `projet`
+--
+ALTER TABLE `projet`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `statut`
+--
+ALTER TABLE `statut`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `tracker`
+--
+ALTER TABLE `tracker`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `notification`
+--
+ALTER TABLE `notification`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `projet`
+--
+ALTER TABLE `projet`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
